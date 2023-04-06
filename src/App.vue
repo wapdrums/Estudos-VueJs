19e183b088ac6d378173d1669f4044565f5b9fcc
WESLEY PLAZA <69492841+wapdrums@users.noreply.github.com>
2023-04-05 21:21:30 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral>
+      </BarraLateral>
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioProjeto></FormularioProjeto>
+    </div>
+</main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import FormularioProjeto from './components/Formulario.vue';
+
+export default defineComponent({
+    name: "App",
+    components: {
+    BarraLateral,
+    FormularioProjeto,
+}
+});
+</script>
+
+<style>
+
+</style>
